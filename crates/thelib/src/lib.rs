19dbc0a2bffe70cf89e8598938aea0108718b484
Glenn Bitar <glennbitar@gmail.com>
2024-04-24 21:36:47 +0200

    Add library crate

---
+++ crates/thelib/src/lib.rs
@@ -0,0 +1,74 @@
+//! Contains functionality for working with prime numbers.
+
+/// Returns `true` if the given number is prime, `false` otherwise.
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
+/// Returns the next prime number after the given number.
+pub fn next_prime(n: u64) -> u64 {
+    let mut n = n + 1;
+    while !is_prime(n) {
+        n += 1;
+    }
+    n
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn is_prime_works() {
+        let expectations = [
+            (1, false),
+            (2, true),
+            (3, true),
+            (4, false),
+            (5, true),
+            (6, false),
+            (7, true),
+            (8, false),
+            (9, false),
+            (10, false),
+            (11, true),
+            (12, false),
+            (13, true),
+        ];
+        for (n, expected) in expectations {
+            assert_eq!(is_prime(n), expected);
+        }
+    }
+    
+    #[test]
+    fn next_prime_works() {
+        let expectations = [
+            (1, 2),
+            (2, 3),
+            (3, 5),
+            (4, 5),
+            (5, 7),
+            (6, 7),
+            (7, 11),
+            (8, 11),
+            (9, 11),
+            (10, 11),
+            (11, 13),
+            (12, 13),
+            (13, 17),
+        ];
+        for (n, expected) in expectations {
+            assert_eq!(next_prime(n), expected);
+        }
+    }
+}
